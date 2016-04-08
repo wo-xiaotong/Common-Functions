450de3e4d70ef007dc365650034903c4f9f12b33
jarvis4tonny <1096232062@qq.com>
2016-04-08 09:27:04 +0800

    add example.c

---
+++ example.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int add(int a,int b)
+{
+	return a+b;
+}
+
+
+int main()
+{
+	printf("%d\n",add(1,1));	
+	return 0;
+}
---
+++ leetcode-others/invert_binary_tree.cpp
@@ -0,0 +1,25 @@
+/*definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)return root;
+        TreeNode *temp;
+        
+        temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+
+        if(root->left!=NULL)invertTree(root->left);
+        if(root->right!=NULL)invertTree(root->right);
+
+        return root;
+    }
+};
