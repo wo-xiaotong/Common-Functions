4901a9b33b2987ecb4c11c49ee244e25c69a45e8
jarvis4tonny <1096232062@qq.com>
2016-06-15 16:07:13 +0800

    add linux ping to ping_test.py

--- Exercise_Py/ping_test.py
+++ Exercise_Py/ping_test.py
@@ -2,22 +2,35 @@
 # -*- coding: utf-8 -*-
 import subprocess
 
-def ping_LAN(ip,begin,end):
-    cmd="cmd.exe"
-    print("----------------begin----------------")
+def linux_ping_LAN(netid,begin,end):
+    command="/bin/sh"
     for i in range(begin,end):
-        p=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
-        p.stdin.write("ping -n 1 -w 1 %s.%d\n" % (ip,i)) #windows
+        p=subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+        p.stdin.write("ping -c 1 -w 1 %s.%d\n" % (netid,i))
+        p.stdin.close()
+        p.wait()
+        out = p.stdout.read()  
+        
+        if "100% packet loss" not in out:
+            print "%s.%d ok!" % (netid,i)
+        else:
+            print "%s.%d failed!" % (netid,i)
+
+
+def win_ping_LAN(netid,begin,end):
+    command="cmd.exe"
+    for i in range(begin,end):
+        p=subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
+        p.stdin.write("ping -n 1 -w 1 %s.%d\n") % (netid,i)
         p.stdin.close()
         p.wait()
         out = p.stdout.read()  
         
         if u"请求超时" not in out.decode("GBK"):
-            print "ip.%d ok!" % i
+            print "%s.%d ok!" % (netid,i)
         else:
-            print "ip.%d failed!" % i
-    print "----------------end----------------"
+            print "%s.%d failed!" % (netid,i)
 
 
 if __name__=="__main__":
-    pingLAN("192.168.159",1,255)
+    linux_ping_LAN("192.168.159",1,255)
